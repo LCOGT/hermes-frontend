bb379205362ba72fd2faf81ed16887cb449d175a
Joseph Chatelain <jchatelain@lco.global>
2022-04-15 11:07:08 -0700

    return to non-literal link

--- src/views/ViewMessages.vue
+++ src/views/ViewMessages.vue
@@ -70,8 +70,6 @@
         </b>
       </template>
     </b-table>
-    {{ items }}
-    <!-- {{ process.env.VUE_APP_HERMES_BACKEND_URL }} -->
   </div>
 </template>
 
@@ -116,7 +114,7 @@ export default {
   },
   mounted() {
     axios
-      .get("http://hermes-dev.lco.gtn/api/v0/messages.json ")
+      .get(process.env.VUE_APP_HERMES_BACKEND_URL)
       .then((response) => (this.items = response.data))
       .catch((error) => console.log(error));
   },
