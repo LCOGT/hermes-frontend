ce7cd84fdb3b00f023343731866df916d6bc4ea2
Joseph Chatelain <jchatelain@lco.global>
2022-04-25 16:23:17 -0700

    fix some spacing on candidate submission page

--- src/views/SubmitCandidates.vue
+++ src/views/SubmitCandidates.vue
@@ -1,43 +1,51 @@
 <template>
   <b-container fluid class="outside-container">
-    <b-row>
-      <b-col>
-        <label for="title-input">Title:</label>
-        <b-form-input class="title-input" v-model="title" placeholder="Title"></b-form-input>
-      </b-col>
-      <b-col>
-        <div>
-          <label for="topic-input">Topic:</label>
-          <b-form-select class="topic-input" v-model="topic" :options="['hermes.test']">Topic</b-form-select>
-        </div>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col class="eventid-col">
-        <label for="eventid-input">Event ID:</label>
-
-        <b-form-input class="eventid-input" v-model="eventid" placeholder="Event ID"></b-form-input>
-      </b-col>
-      <b-col class="authors-col">
-        <label for="authors-input">Authors:</label>
-        <b-form-input class="authors-input" v-model="author" placeholder="Authors"></b-form-input>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col class="candidates-input-col">
-      <candidates-input-table></candidates-input-table>
-      </b-col>
-      <b-col class="extra-input-col">
-        <label for="extra-input-table">Additional Data Elements:</label>
-        <additional-data-input-table class="extra-input-table"></additional-data-input-table>
-      </b-col>
-    </b-row>
+    <!-- Form Title -->
+    <h1>Candidate Submission Form</h1>
+    <!-- Basic Info Form -->
+    <b-card>
+      <b-row class=p-2>
+        <b-col>
+          <label for="title-input">Title:</label>
+          <b-form-input class="title-input" v-model="title" placeholder="Title"></b-form-input>
+        </b-col>
+        <b-col>
+          <div>
+            <label for="topic-input">Topic:</label>
+            <b-form-select class="topic-input" v-model="topic" :options="['hermes.test']">Topic</b-form-select>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row class=p-2>
+        <b-col class="eventid-col">
+          <label for="eventid-input">Event ID:</label>
+          <b-form-input class="eventid-input" v-model="eventid" placeholder="Event ID"></b-form-input>
+        </b-col>
+        <b-col class="authors-col">
+          <label for="authors-input">Authors:</label>
+          <b-form-input class="authors-input" v-model="author" placeholder="Authors"></b-form-input>
+        </b-col>
+      </b-row>
+    </b-card>
+    <!-- Data Tables -->
+    <b-card>
+      <b-row>
+        <b-col class="candidates-input-col">
+        <candidates-input-table></candidates-input-table>
+        </b-col>
+        <b-col class="extra-input-col">
+          <label for="extra-input-table">Additional Data Elements:</label>
+          <additional-data-input-table class="extra-input-table"></additional-data-input-table>
+        </b-col>
+      </b-row>
+    </b-card>
+    <!-- Message Form -->
     <b-row class="py-3">
       <label for="message-input">Message:</label>
       <b-form-textarea v-model="message" id="message-input" placeholder="Message" rows="3" max-rows="6"></b-form-textarea>
     </b-row>
+    <!-- Submit -->
     <b-row>
-      
       <b-col>
       <div class="submit-container">
         <b-button class="submit-button" variant="success" @click="submitToHop">Submit</b-button>
@@ -129,11 +137,13 @@ export default {
   max-width: 20%;
   padding: 0;
   padding-right: 3%;
+  padding-left: 15px;
 }
 
 .authors-col {
   max-width: 100%;
   padding: 0;
+  padding-right: 15px;
 }
 
 .extra-input-col {
