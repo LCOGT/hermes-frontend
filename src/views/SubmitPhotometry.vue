7ded264e9b2d827cd1261eec7b32ce0dd3f8dca5
Joseph Chatelain <jchatelain@lco.global>
2022-04-28 13:21:48 -0700

    update photometry page styling to match Candidate page

--- src/views/SubmitPhotometry.vue
+++ src/views/SubmitPhotometry.vue
@@ -1,33 +1,38 @@
 <template>
   <b-container fluid class="outside-container">
-    <b-row>
-    <b-col>
+    <!-- Form Title -->
+    <h1>Photometry Reporting Form</h1>
+    <!-- Basic Info Form -->
       <b-row>
-        <b-col>
-        <label for="title-input">Title:</label>
-        <b-form-input class="title-input" v-model="title" placeholder="Title"></b-form-input>
+      <b-col class=pr-1>
+        <b-card class="mb-2 shadow" border-variant="primary">
+          <b-row class=p-2>
+            <b-col>
+              <label for="title-input">Title:</label>
+              <b-form-input class="title-input" v-model="title" placeholder="Title"></b-form-input>
+            </b-col>
+            <b-col>
+              <div>
+                <label for="topic-input">Topic:</label>
+                <b-form-select class="topic-input" v-model="topic" :options="['hermes.test']">Topic</b-form-select>
+              </div>
+            </b-col>
+          </b-row>
+          <b-row class=p-2>
+            <b-col class="eventid-col">
+              <label for="eventid-input">Event ID:</label>
+              <b-form-input class="eventid-input" placeholder="Event ID"></b-form-input>
+            </b-col>
+            <b-col class="authors-col">
+              <label for="authors-input">Authors:</label>
+              <b-form-input class="authors-input" placeholder="Authors"></b-form-input>
+            </b-col>
+          </b-row>
+        </b-card>
       </b-col>
-      <b-col>
-        <div>
-          <label for="topic-input">Topic:</label>
-          <b-form-select class="topic-input" v-model="topic" :options="['hermes.test']">Topic</b-form-select>
-        </div>
-      </b-col>
-      </b-row>
-      <b-row>
-        <b-col class="eventid-col">
-          <label for="eventid-input">Event ID:</label>
-
-          <b-form-input class="eventid-input" placeholder="Event ID"></b-form-input>
-        </b-col>
-        <b-col class="authors-col">
-          <label for="authors-input">Authors:</label>
-          <b-form-input class="authors-input" placeholder="Authors"></b-form-input>
-        </b-col>
-      </b-row>
-    </b-col>
-      <b-col>
-        <b-card title="Upload Data">
+      <b-col class=pl-1>
+        <!-- Upload Data Card -->
+        <b-card title="Upload Data" class="upload-card mb-2 shadow" border-variant="primary">
           <b-card-text>
             This space will contain information explaining the procedure for uploading a text file that will automatically fill the photometry table below.
           </b-card-text>
@@ -35,6 +40,8 @@
         </b-card>
       </b-col>
     </b-row>
+    <!-- Data Tables -->
+    <b-card class="mb-2 shadow" border-variant="primary">
     <b-row>
       <b-col class="photometry-input-col">
       <photometry-input-table></photometry-input-table>
@@ -44,14 +51,19 @@
         <additional-data-input-table class="extra-input-table"></additional-data-input-table>
       </b-col>
     </b-row>
-    <b-row class="py-3">
+    </b-card>
+    <!-- Message Form -->
+    <b-card class="mb-2 shadow" border-variant="primary">
+    <b-row class="p-3">
       <label for="message-input">Message:</label>
       <b-form-textarea id="message-input" placeholder="Message" rows="3" max-rows="6"></b-form-textarea>
     </b-row>
+    </b-card>
+    <!-- Submit -->
     <b-row>
       <b-col>
       <div class="submit-container">
-        <b-button class="submit-button" variant="success" @click="submitToHop">Submit</b-button>
+        <b-button class="submit-button shadow" variant="success" @click="submitToHop">Submit</b-button>
       </div>
       </b-col>
     </b-row>
@@ -97,11 +109,13 @@ export default {
   max-width: 20%;
   padding: 0;
   padding-right: 3%;
+  padding-left: 15px;
 }
 
 .authors-col {
   max-width: 100%;
   padding: 0;
+  padding-right: 15px;
 }
 
 .extra-input-col {
@@ -112,5 +126,8 @@ export default {
 }
 .import-button {
   color: white
+}
+.upload-card {
+  height: 96%;
 }
 </style>
