48156dbb04adfbeb6cb975307cd6d4ecea801a46
Joseph Chatelain <jchatelain@lco.global>
2022-06-02 13:38:16 -0700

    add column order for maindata to payload

--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -247,6 +247,11 @@ export default {
       catch {
         return
       }
+      // get Main Data column order
+      let mainDataOrder = [];
+      for (const [key, ] of Object.entries(mainData[0])) {
+        mainDataOrder.push(key);
+      }
       // Build Basic Payload to match backend Model Structure
       let payload = {
         "topic": this.topic,
@@ -260,6 +265,9 @@ export default {
         payload.data[this.getMainTableName] = mainData;
       } else {
         payload.data['main_data'] = mainData;
+      }
+      if (mainDataOrder) {
+        payload.data.mainDataOrder = mainDataOrder;
       }
       if (this.eventid) {
         payload.data.eventid = this.eventid;
